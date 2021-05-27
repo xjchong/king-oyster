08e3143fb5413fd372ac7ce03caa8796b802ed0b
Xavier <xavier.chong.j@gmail.com>
2021-05-27 08:40:09 -0400

    Move ai logging outside of GD console

--- src/main/kotlin/com/helloworldramen/kingoyster/ai/Ai.kt
+++ src/main/kotlin/com/helloworldramen/kingoyster/ai/Ai.kt
@@ -1,8 +1,6 @@
 package com.helloworldramen.kingoyster.ai
 
 import com.helloworldramen.kingoyster.ai.considerations.ConstantConsideration
-import com.helloworldramen.kingoyster.ai.considerations.IsEnemyInRangeConsideration
-import com.helloworldramen.kingoyster.ai.considerations.IsEnemyInSightConsideration
 import com.helloworldramen.kingoyster.ai.strategies.AttackInRangeEnemiesStrategy
 import com.helloworldramen.kingoyster.ai.strategies.ChaseVisibleEnemiesStrategy
 import com.helloworldramen.kingoyster.ai.strategies.IdleStrategy
@@ -11,7 +9,6 @@ import com.helloworldramen.kingoyster.ai.reasoners.HighestValueReasoner
 import com.helloworldramen.kingoyster.ai.reasoners.PurelyRandomReasoner
 import com.helloworldramen.kingoyster.oyster.Context
 import com.helloworldramen.kingoyster.oyster.Entity
-import godot.global.GD
 
 object Ai {
 
@@ -59,7 +56,7 @@ object Ai {
 
     private fun log(entity: Entity, optionsWithScore: List<Pair<GameAiOption, Double>>, startTime: Long) {
         val totalTimeString = String.format("%.2f", (System.nanoTime() - startTime) / 1000000.0)
-        GD.print("AI(${totalTimeString}ms) ${entity.name}: ${optionsWithScore.map { (option, score) ->
+        println("AI(${totalTimeString}ms) ${entity.name}: ${optionsWithScore.map { (option, score) ->
             Pair("${option.parentStrategy.tag}-${option.tag}", String.format("%.2f", score))
         }}")
     }
