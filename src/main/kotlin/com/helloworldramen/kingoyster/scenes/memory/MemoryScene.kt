2382c5343d917e7d0b3138fb072b90ae12e61b93
Xavier <xavier.chong.j@gmail.com>
2021-06-03 21:38:48 -0400

    Add fade effect for memory tiles

--- src/main/kotlin/com/helloworldramen/kingoyster/scenes/memory/MemoryScene.kt
+++ src/main/kotlin/com/helloworldramen/kingoyster/scenes/memory/MemoryScene.kt
@@ -10,59 +10,98 @@ import com.helloworldramen.kingoyster.scenes.entity.EntitySprite
 import godot.ColorRect
 import godot.Label
 import godot.Node2D
+import godot.Tween
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
+import godot.core.Color
+import godot.core.NodePath
 import godot.extensions.getNodeAs
 
 @RegisterClass
 class MemoryScene : Node2D() {
 
 	private val memoryRect: ColorRect by lazy { getNodeAs("MemoryRect")!! }
-	private val fogRect: ColorRect by lazy { getNodeAs("FogRect")!! }
 	private val entitySprite: EntitySprite by lazy { getNodeAs("EntitySprite")!! }
 	private val durabilityLabel: Label by lazy { getNodeAs("EntitySprite/DurabilityLabel")!! }
+	private val tween: Tween by lazy { getNodeAs("Tween")!! }
 
 	private var entity: Entity = Entity.UNKNOWN
 	private var worldPosition: Position = Position(0, 0)
 
+	private var lastState: MemoryState = MemoryState.Unknown
+
 	@RegisterFunction
 	override fun _process(delta: Double) {
-		updateAppearance()
+		val memoryState = nextMemoryState()
+		
+		if (lastState != memoryState) {
+			lastState = memoryState
+			updateAppearance()
+		}
 	}
 
 	fun bind(entity: Entity, worldPosition: Position) {
 		this.entity = entity
 		this.worldPosition = worldPosition
+		updateAppearance()
 	}
 
 	private fun updateAppearance() {
-		val visiblePositions = entity.visiblePositions()
-		val rememberedEntities = entity.find(MemoryPart::class)?.get(worldPosition)
-
-		when {
-			visiblePositions.contains(worldPosition) -> { // Visible tile.
-				visible = false
-			}
-			rememberedEntities == null -> { // Unvisited (fog) tile.
-				visible = true
-				entitySprite.hide()
-				fogRect.visible = true
-			}
-			else -> { // Memory tile.
-				val topEntity = rememberedEntities.lastOrNull()
-
-				visible = true
-				entitySprite.bind(topEntity)
-				durabilityLabel.visible = topEntity?.has<WeaponPart>() == true
-				durabilityLabel.text = topEntity?.durability().toString()
-				entitySprite.show()
-
-				fogRect.visible = false
-			}
+		when (lastState) {
+			MemoryState.Visible -> animateVisible()
+			MemoryState.Memory -> animateMemory()
+			MemoryState.Unknown -> animateUnknown()
 		}
+	}
+
+	private fun nextMemoryState(): MemoryState {
+		val visiblePositions = entity.visiblePositions()
+		val rememberedEntities = entity.find<MemoryPart>()?.get(worldPosition)
+
+		return when {
+			visiblePositions.contains(worldPosition) -> MemoryState.Visible
+			rememberedEntities == null -> MemoryState.Unknown
+			else -> MemoryState.Memory
+		}
+	}
+
+	private fun animateVisible() {
+		entitySprite.hide()
+        tweenModulate(0.0)
+	}
+
+	private fun animateMemory() {
+		val topEntity = entity.find<MemoryPart>()?.get(worldPosition)?.lastOrNull()
+
+		entitySprite.bind(topEntity)
+		entitySprite.show()
+
+		durabilityLabel.visible = topEntity?.has<WeaponPart>() == true
+		durabilityLabel.text = topEntity?.durability().toString()
+
+        tweenModulate(0.5)
+	}
+
+	private fun animateUnknown() {
+		entitySprite.hide()
+        tweenModulate(1.0)
+	}
+
+	private fun tweenModulate(alpha: Double) {
+		tween.interpolateProperty(memoryRect, NodePath("color"),
+			initialVal = memoryRect.color, finalVal = Color(0.0, 0.0, 0.0, alpha),
+			duration = 0.2, Tween.TRANS_QUAD, Tween.EASE_IN
+		)
+		tween.start()
 	}
 
 	companion object {
 		const val PATH = "res://src/main/kotlin/com/helloworldramen/kingoyster/scenes/memory/MemoryScene.tscn"
+	}
+
+	sealed class MemoryState {
+		object Unknown : MemoryState()
+		object Memory : MemoryState()
+		object Visible : MemoryState()
 	}
 }
