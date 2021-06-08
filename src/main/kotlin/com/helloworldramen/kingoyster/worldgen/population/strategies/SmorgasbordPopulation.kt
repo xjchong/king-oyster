d56277258fc66eac447bf5d205d0cfa8be2a3f21
Xavier <xavier.chong.j@gmail.com>
2021-06-08 07:08:19 -0400

    Refactor smorgasbord population strategy

--- src/main/kotlin/com/helloworldramen/kingoyster/worldgen/population/strategies/SmorgasbordPopulation.kt
+++ src/main/kotlin/com/helloworldramen/kingoyster/worldgen/population/strategies/SmorgasbordPopulation.kt
@@ -8,21 +8,18 @@ import com.helloworldramen.kingoyster.worldgen.population.PopulationTemplate
 import com.helloworldramen.kingoyster.worldgen.population.rules.StairsPopulationRule
 
 class SmorgasbordPopulation(densityFactor: Double = 1.0) : PopulationStrategy(
-    PopulationTemplate(PopulationRule(ActorFactory.goblin()))
-        .withDensity(0.04 * densityFactor),
-    PopulationTemplate(PopulationRule(ActorFactory.blueSlime()))
-        .withDensity(0.03 * densityFactor),
-    PopulationTemplate(PopulationRule(ActorFactory.redSlime()))
-        .withDensity(0.01 * densityFactor),
+    PopulationTemplate(
+        100 to PopulationRule(ActorFactory.goblin()),
+        100 to PopulationRule(ActorFactory.blueSlime()),
+        100 to PopulationRule(ActorFactory.redSlime()),
+    ).withDensity(0.1 * densityFactor),
     PopulationTemplate(PopulationRule(ActorFactory.ghost()))
         .withCount(1 * densityFactor, 2 * densityFactor),
-    PopulationTemplate(PopulationRule(WeaponFactory.dagger()))
-        .withCount(2 * densityFactor, 4 * densityFactor),
-    PopulationTemplate(PopulationRule(WeaponFactory.longsword()))
-        .withCount(2 * densityFactor, 4 * densityFactor),
-    PopulationTemplate(PopulationRule(WeaponFactory.greatsword()))
-        .withCount(2 * densityFactor, 4 * densityFactor),
-    PopulationTemplate(PopulationRule(WeaponFactory.rapier()))
-        .withCount(2 * densityFactor, 4 * densityFactor),
+    PopulationTemplate(
+        100 to PopulationRule(WeaponFactory.dagger()),
+        100 to PopulationRule(WeaponFactory.longsword()),
+        100 to PopulationRule(WeaponFactory.greatsword()),
+        100 to PopulationRule(WeaponFactory.rapier()),
+    ).withCount(8 * densityFactor, 16 * densityFactor),
     PopulationTemplate(StairsPopulationRule()).withCount(1)
 )
