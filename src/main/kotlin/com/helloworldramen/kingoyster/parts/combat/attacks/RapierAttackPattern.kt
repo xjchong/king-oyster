4b4c9af44c186186ec3c5ddb6f4367cf3f1b967f
Xavier <xavier.chong.j@gmail.com>
2021-06-09 08:33:16 -0400

    Increase rapier lunge damage multiplier

--- src/main/kotlin/com/helloworldramen/kingoyster/parts/combat/attacks/RapierAttackPattern.kt
+++ src/main/kotlin/com/helloworldramen/kingoyster/parts/combat/attacks/RapierAttackPattern.kt
@@ -39,7 +39,7 @@ class RapierAttackPattern(
 
         return getHitPositions(context, entity, direction).associateWith {
             // Rapier does more damage when lunging.
-            val distanceFactor = if (currentPosition.distanceFrom(it) > 1) 1.5 else 1.0
+            val distanceFactor = if (currentPosition.distanceFrom(it) > 1) 2.0 else 1.0
 
             DamageInfo(powerFactor * distanceFactor, damageType, elementType)
         }
